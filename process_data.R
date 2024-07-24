5caff1e3851f57d9e4b6604be2d277492f8866fa
GarikPar <garik.paramazyan06@gmail.com>
2024-07-24 15:18:30 +0400

    Initial

--- process_data.R
+++ process_data.R
@@ -1,5 +1,22 @@
 library(tidyverse)
+library(knitr)
 
-D_diamonds <- diamonds |> 
-  filter(z != 0) |> 
-  filter(color == "D") 
+vazgen <- read_csv("vazgen.csv")
+maralik <- read_csv("maralik.csv")
+
+merged <- rbind(vazgen, maralik)
+
+
+V_vazgen <- vazgen|>
+  select(time, temperature)|>
+   arrange(desc(temperature))
+
+temp_plot <- merged|>
+  ggplot(mapping = aes(x = time, y = temperature, color = device)) +
+  geom_line(na.rm = TRUE) +
+  geom_smooth(na.rm = TRUE)
+ 
+
+
+print(temp_plot)
+
