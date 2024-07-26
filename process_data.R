794d85edd74937e97647b2134ac53423c51f5f2d
GarikPar <garik.paramazyan06@gmail.com>
2024-07-26 12:29:12 +0400

    Final Project

--- process_data.R
+++ process_data.R
@@ -30,8 +30,12 @@ hum_vazgen <- vazgen|>
 
 hum_plot <- vazgen|>
   ggplot(mapping = aes(x = time, y = humidity, color = "blue")) +
-  #geom_line(na.rm = TRUE, color = "steelblue") +
-  geom_smooth(na.rm = TRUE, color = "steelblue")
+  geom_smooth(na.rm = TRUE, color = "steelblue") + 
+  labs(title = "Humidity in Yerevan from November 2023 to July 2024",
+       x = "Months",
+       y = "Humidity")
+ggsave(hum_plot, file = "size_v_hum.png")
+
 
 #print(hum_plot)
 
@@ -42,8 +46,11 @@ press_vazgen <- vazgen|>
 press_plot <- vazgen|>
   ggplot(mapping = aes(x = time, y = pressure, color = device)) +
   #geom_line(na.rm = TRUE, color = "orange") +
-  geom_smooth(na.rm = TRUE, color = "orange")
-
+  geom_smooth(na.rm = TRUE, color = "orange") +
+  labs(title = "Pressure measurements in Yerevan from November 2023 to July 2024",
+       x = "Months",
+       y = "Pressure")
+ggsave(press_plot, file = "size_v_press.png")
 #print(press_plot)
 
 pm1_vazgen <- vazgen|>
@@ -52,9 +59,12 @@ pm1_vazgen <- vazgen|>
 
 pm1_plot <- vazgen|>
   ggplot(mapping = aes(x = time, y = pm1, color = device)) +
-  #geom_line(na.rm = TRUE, color = "black") +
-  geom_smooth(na.rm = TRUE, color = "black")
+  geom_smooth(na.rm = TRUE, color = "black") +
+  labs(title = "Air quality(pm1) in Yerevan from November 2023 to July 2024",
+       x = "Months",
+       y = "Air Quality(pm1)")
 
+ggsave(pm1_plot, file = "size_v_pm1.png")
 #print(pm1_plot)
 
 pm2_5_vazgen <- vazgen|>
@@ -64,8 +74,11 @@ pm2_5_vazgen <- vazgen|>
 pm2_5_plot <- vazgen|>
   ggplot(mapping = aes(x = time, y = pm2_5, color = device)) +
  # geom_line(na.rm = TRUE, color = "#00AFBB") +
-  geom_smooth(na.rm = TRUE, color = "#00AFBB")
-
+  geom_smooth(na.rm = TRUE, color = "#00AFBB") +
+  labs(title = "Air quality(pm2_5) in Yerevan from November 2023 to July 2024",
+       x = "Months",
+       y = "Air Quality(pm2_5)")
+ggsave(pm2_5_plot, file = "size_v_pm2_5.png")
 #print(pm2_5_plot)
 
 pm10_vazgen <- vazgen|>
@@ -75,11 +88,19 @@ pm10_vazgen <- vazgen|>
 pm10_plot <- vazgen|>
   ggplot(mapping = aes(x = time, y = pm10, color = device)) +
   #geom_line(na.rm = TRUE, color = "#C4961A") +
-  geom_smooth(na.rm = TRUE, color = "#C4961A")
-
+  geom_smooth(na.rm = TRUE, color = "#C4961A") +
+  labs(title = "Air quality(pm10) in Yerevan from November 2023 to July 2024",
+       x = "Months",
+       y = "Air Quality(pm10)")
+ggsave(pm10_plot, file = "size_v_pm10.png")
 #print(pm10_plot)
 
-write_rds(temp_plot, file = "clean_temp.rds")
+#write_rds(temp_plot, file = "clean_temp.rds")
+#ggsave(hum_plot, "size_v_hum.png")
+#ggsave( press_plot, "size_v_press.png",)
+#ggsave( pm1_plot, "size_v_pm1.png",)
+#ggsave(pm2_5_plot, "size_v_pm2_5.png")
+#ggsave(pm10_plot, "size_v_pm10.png",)
 
 
 
